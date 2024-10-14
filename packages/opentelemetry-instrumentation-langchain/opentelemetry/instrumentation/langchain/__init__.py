bdadcbdeb22cb6a9eef748957b58d7c6fab94660
Ronen Schaffer <ronen.schaffer@ibm.com>
2024-10-14 17:16:24 +0300

    Add trace context to client requests

--- packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/__init__.py
+++ packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/__init__.py
@@ -36,14 +36,22 @@ class LangchainInstrumentor(BaseInstrumentor):
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
 
+        traceloopCallbackHandler = TraceloopCallbackHandler(tracer)
         wrap_function_wrapper(
             module="langchain_core.callbacks",
             name="BaseCallbackManager.__init__",
-            wrapper=_BaseCallbackManagerInitWrapper(TraceloopCallbackHandler(tracer)),
+            wrapper=_BaseCallbackManagerInitWrapper(traceloopCallbackHandler),
+        )
+
+        wrap_function_wrapper(
+            module="langchain_community.llms.openai",
+            name="completion_with_retry",
+            wrapper=_CompletionWithRetryWrapper(traceloopCallbackHandler),
         )
 
     def _uninstrument(self, **kwargs):
         unwrap("langchain_core.callbacks", "BaseCallbackManager.__init__")
+        unwrap("langchain_community.llms.openai", "completion_with_retry")
 
 
 class _BaseCallbackManagerInitWrapper:
@@ -63,3 +71,32 @@ class _BaseCallbackManagerInitWrapper:
                 break
         else:
             instance.add_handler(self._callback_manager, True)
+
+
+class _CompletionWithRetryWrapper:
+    def __init__(self, callback_manager: "TraceloopCallbackHandler"):
+        self._callback_manager = callback_manager
+
+    def __call__(
+        self,
+        wrapped,
+        instance,
+        args,
+        kwargs,
+    ) -> None:
+        from opentelemetry.trace.propagation.tracecontext import (
+            TraceContextTextMapPropagator,
+        )
+        from opentelemetry.trace.propagation import set_span_in_context
+
+        run_id = kwargs["run_manager"].run_id
+        span_holder = self._callback_manager.spans[run_id]
+
+        extra_headers = kwargs.get("extra_headers", {})
+
+        ctx = set_span_in_context(span_holder.span)
+        TraceContextTextMapPropagator().inject(extra_headers, context=ctx)
+        # TraceContextTextMapPropagator().inject(extra_headers, context=span_holder.context)
+
+        kwargs["extra_headers"] = extra_headers
+        return wrapped(*args, **kwargs)
